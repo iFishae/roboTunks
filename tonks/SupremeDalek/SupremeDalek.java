a04fc06d3679d0c938ffa066e08db001759c341b
Deryn <62044922+iFishae@users.noreply.github.com>
2022-06-02 03:22:50 -0400

    I SPENT TOO LONG ON MAKIGN A SHITTUY BOT FUCK MEEE

--- tonks/SupremeDalek/SupremeDalek.java
+++ tonks/SupremeDalek/SupremeDalek.java
@@ -4,48 +4,71 @@ import java.awt.Color;
 
 // API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
 
-/**
- * K9 - a robot by (your name here)
- */
 public class SupremeDalek extends Robot {
+
+	boolean imaboolean;
+	boolean peek;
+	double moveAmount;
+	int safe = 0; // How many times bot has been hit by a bullet
+	int tsbh = 0; // Turns since bullet hit
+	
+
+
 	public void run() {
 		setBodyColor(new Color(129,30,42));
-		setGunColor(new Color(215,215,217)); //215,215,217
+		setGunColor(new Color(215,215,217));
 		setRadarColor(new Color(217,213,210));
 		setBulletColor(new Color(173,212,254));
 		setScanColor(new Color(217,213,210));
 		
-		// Scanning at the beginning of the round, to check for enemy positions	
-		scan();
+		moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
+		peek = false;
+		imaboolean = false;
+
+		turnLeft(getHeading() % 90);
+		ahead(moveAmount);
+		peek = true;
+		turnRight(90);
+		turnGunRight(90);
+
 		
 		while(true) {
-			ahead(100);			
+		ahead(moveAmount);
+		imaboolean = true;
+		turnGunRight(90);
+		scan();
+		safe++;
+		turnGunLeft(90);
+		imaboolean = false;
+		back(moveAmount);
+		imaboolean = true;
+		turnGunLeft(90);
+		scan();
+		imaboolean = false;
+		}
+	}
+	
+	public void onScannedRobot(ScannedRobotEvent e) {
+		fire(2);
+		scan();
+	}
+	
+
+		
+	public void onHitRobot(HitRobotEvent e) {
+			if (e.getBearing() > -90 && e.getBearing() < 90) {
 			back(100);
-			
+		}
+		else {
+			ahead(100);
 		}
 	}
 
-	/**
-	 * onScannedRobot: What to do when you see another robot
-	 */
-	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		fire(1);
+	public void onHitByBullet(HitByBulletEvent e) {
+		if (safe > 4) {
+			ahead(100);
+		}
 	}
 
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
-	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(10);
-	}
-	
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
-	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
-	}	
-}
+} // "AAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH" - Deryn (2022: Trying to defeat Walls)
+// "Nvm i got it" - Deryn (2022: Five minutes later)
